58216b03a508c948c3d082d848bb81661f699175
wenyan.fan <wenyan.fan@weimob.com>
2019-12-16 13:35:26 +0800

    改变原数组对象某一属性值方法

--- pages/question/question.js
+++ pages/question/question.js
@@ -10,6 +10,19 @@ Page({
     this.setData({
       testTitle:app.globalData.testTitle,
     })
-  }
+  },
+
+  //改变数组对象某一属性值
+  // progressList.forEach((item,index,attr)=>{
+  //   var allItem="progressList["+index+"].isPass";
+  //   this.setData({
+  //     [allItem]:"false"
+  //   })
+  //   if(index==selectLevelid){
+  //     this.setData({
+  //       [allItem]:"true"
+  //     })
+  //   }
+  // })
   
 })
